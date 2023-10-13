c7c38fd361c8a622282ec48e9359f705af4c0208
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2023-10-13 03:41:50 +0000

    Update uv.config.js

--- Ultraviolet-Static/public/uv/uv.config.js
+++ Ultraviolet-Static/public/uv/uv.config.js
@@ -1,13 +1,13 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-  prefix: "https://math.madroid.design/uv/service/",
+  prefix: "/uv/service/",
   bare: "https://math.madroid.design/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "https://math.madroid.design/uv/uv.handler.js",
-  client: "https://math.madroid.design/uv/uv.client.js",
-  bundle: "https://math.madroid.design/uv/uv.bundle.js",
-  config: "https://math.madroid.design/uv/uv.config.js",
-  sw: "https://math.madroid.design/uv/uv.sw.js",
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
 };
